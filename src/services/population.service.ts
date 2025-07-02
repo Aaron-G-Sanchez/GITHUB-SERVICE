ba8ca212c6846174262e899af0dfd212710b3102
Aaron Sanchez <saarong12@gmail.com>
2025-07-02 15:43:14 -0500

    Added functionality to save a users repositories to the database.

--- src/services/population.service.ts
+++ src/services/population.service.ts
@@ -1,8 +1,9 @@
 import { GITHUB_USER_REPOS_URL } from '../lib/constants.lib'
 import { FetchUserRepos } from './utils/github.util'
 import { connect, collections } from '../database/db'
+import { MongoClient } from 'mongodb'
 
-const populateDatabase = async () => {
+export const PopulateDatabase = async (): Promise<MongoClient> => {
   const client = await connect()
 
   const repositories = await FetchUserRepos(GITHUB_USER_REPOS_URL)
@@ -12,9 +13,7 @@ const populateDatabase = async () => {
     console.log('Repositories written to DB: ', repositories.length)
   } catch (err) {
     console.error('Error writing to DB: ', err)
-  } finally {
-    client.close()
   }
-}
 
-populateDatabase()
+  return client
+}

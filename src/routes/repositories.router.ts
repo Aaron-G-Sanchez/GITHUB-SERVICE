7bfd3268ffc5e6d56540c1163ad24a849667f63c
Aaron Sanchez <saarong12@gmail.com>
2025-07-14 20:51:00 -0500

    Created route to get repository by id.

--- src/routes/repositories.router.ts
+++ src/routes/repositories.router.ts
@@ -16,3 +16,30 @@ repositoriesRouter.get('/', async (_req: Request, res: Response) => {
     res.status(500).send(err)
   }
 })
+
+repositoriesRouter.get('/:id', async (req: Request, res: Response) => {
+  // TODO: Move to separate directory.
+  const idParam = req.params.id
+
+  const id = parseInt(idParam)
+
+  if (isNaN(id) || id < 0) {
+    res.status(400).send({ error: 'Invalid or missing ID' })
+    return
+  }
+
+  try {
+    const repo = await collections.repositories?.findOne({
+      gh_id: id
+    })
+
+    if (!repo) {
+      res.status(404).json({ msg: `No resource for given id: ${id}` })
+      return
+    }
+
+    res.status(200).json({ repo })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})

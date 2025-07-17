6bfea7524a22f4bf05121306c5d2d6e491dfdcba
Aaron Sanchez <saarong12@gmail.com>
2025-07-16 21:17:12 -0500

    Created repository service.
    Tests modified in the following commit.

--- src/routes/repositories.router.ts
+++ src/routes/repositories.router.ts
@@ -1,45 +1,51 @@
 import { Request, Response, Router, json } from 'express'
 
-import { collections } from '@database/db'
+import { RepositoryService } from '@services/repository.service'
 
-export const repositoriesRouter = Router()
+export const CreateRepositoryRouter = (
+  repositoriesService: RepositoryService
+): Router => {
+  const repositoriesRouter = Router()
 
-repositoriesRouter.use(json())
+  repositoriesRouter.use(json())
 
-repositoriesRouter.get('/', async (_req: Request, res: Response) => {
-  try {
-    // TODO: Move to separate services directory.
-    const repos = await collections.repositories?.find({}).toArray()
+  repositoriesRouter.get('/', async (_req: Request, res: Response) => {
+    try {
+      // TODO: Move to separate services directory.
+      const repos = await repositoriesService.getRepositories()
 
-    res.status(200).send({ repos })
-  } catch (err) {
-    res.status(500).send(err)
-  }
-})
+      res.status(200).send({ repos })
+    } catch (err) {
+      const message =
+        err instanceof Error ? err.message : 'Error fetching repositories'
+      res.status(500).send({ error: message })
+    }
+  })
 
-repositoriesRouter.get('/:id', async (req: Request, res: Response) => {
-  // TODO: Move to separate directory.
-  const idParam = req.params.id
+  repositoriesRouter.get('/:id', async (req: Request, res: Response) => {
+    // TODO: Move to separate directory.
+    const idParam = req.params.id
 
-  const id = parseInt(idParam)
+    const id = parseInt(idParam)
 
-  if (isNaN(id) || id < 0) {
-    res.status(400).send({ error: 'Invalid or missing ID' })
-    return
-  }
-
-  try {
-    const repo = await collections.repositories?.findOne({
-      gh_id: id
-    })
-
-    if (!repo) {
-      res.status(404).json({ msg: `No resource for given id: ${id}` })
+    if (isNaN(id) || id < 0) {
+      res.status(400).send({ error: 'Invalid or missing ID' })
       return
     }
 
-    res.status(200).json({ repo })
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
+    try {
+      const repo = await repositoriesService.getRepositoriesById(id)
+
+      if (!repo) {
+        res.status(404).json({ msg: `No resource for given id: ${id}` })
+        return
+      }
+
+      res.status(200).json({ repo })
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  })
+
+  return repositoriesRouter
+}

75d36d5508e02e2e08728107dc400bde942e786c
Aaron Sanchez <saarong12@gmail.com>
2025-07-31 20:24:10 -0500

    Added logic to validate GitHub action received.

--- src/routes/webhooks/webhooks.router.ts
+++ src/routes/webhooks/webhooks.router.ts
@@ -12,11 +12,30 @@ export const CreateWebhookRouter = (
 
   webhookRouter.post('/', async (req: Request, res: Response) => {
     // TODO: Authenticate request.
-    console.log(req.body)
+    const { action } = req.body
 
     // TODO: Implement route.
-    // Determine what action was completed.
-    // - opened, closed,edited, deleted
+    if (!validateAction(action)) {
+      res.status(400).send({ error: 'Missing or incorrect action' })
+      return
+    }
+
+    switch (action) {
+      case GithubAction.Opened:
+        console.log('OPENED')
+        break
+      case GithubAction.Closed:
+        console.log('CLOSED')
+        break
+      case GithubAction.Deleted:
+        console.log('DELETED')
+        break
+      case GithubAction.Edited:
+        console.log('EDITED')
+        break
+      default:
+        break
+    }
     // Parse out the issue details that need to be added (name, url, etc)
     // Find the parent repo by gh_id
     // Push the issue into the parent repos `issues` list
@@ -25,4 +44,10 @@ export const CreateWebhookRouter = (
   })
 
   return webhookRouter
+}
+
+// TODO: Move to separate directory.
+const validateAction = (action: string) => {
+  console.log(action)
+  return Object.values(GithubAction).includes(action as GithubAction)
 }

56741f1bcb6eed424b7b4fff2d670a6e0e3fcfef
Aaron Sanchez <saarong12@gmail.com>
2025-08-26 20:14:23 -0500

    Began work on aurgument processing.

--- scripts/populate.script.ts
+++ scripts/populate.script.ts
@@ -1,8 +1,20 @@
+import { config } from '@config/config.config'
 import { PopulateDatabase } from '@jobs/populate/population.job'
 
 // TODO: Add a dry run flag.
 // parse flag
 // pass flag to PopulateDatabase.
+
+const args = process.argv
+
+/** EVALUATE ARGS */
+if (args.length <= 2) {
+  console.info(
+    `Running in default configuration for environment: ${config.environment}`
+  )
+}
+// Evaluate optional args.
+
 PopulateDatabase()
   .then((client) => {
     console.log('Database population complete')
